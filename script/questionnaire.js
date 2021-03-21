9a852edee2f748110c4581d4fdb9022b8332e12b
Kathrin Janowski <science@kathrinjanowski.com>
2021-03-21 12:56:03 +0100

    Ergebnisse nach klinisch ja/nein

--- script/questionnaire.js
+++ script/questionnaire.js
@@ -8,6 +8,8 @@ gatheredInfo.questions['phq_02'] = 0;
 gatheredInfo.exclude = [];
 
 questionIDs = ['gad_01', 'gad_02', 'phq_01', 'phq_02']
+gadCutoff = 3;
+phqCutoff = 3;
 
 var counter=1;
 
@@ -19,14 +21,41 @@ function updateQuestion(questionID, score)
 function showResult()
 {
 	document.getElementById("frage_"+counter).style.display="none";
+
+	var gadScore = gatheredInfo.questions['gad_01']+gatheredInfo.questions['gad_02'];
+	var phqScore = gatheredInfo.questions['phq_01']+gatheredInfo.questions['phq_02'];
+	var klinisch = (gadScore>=gadCutoff) || (phqScore>=phqCutoff);
 	
-	var text = "score:";
-	for(id of questionIDs){
-		text = text+"\n"+id+": "+gatheredInfo.questions[id];
+	var text = "GAD score: "+gadScore+", PHQ score: "+phqScore+" -> klinisch: "+klinisch;
+	
+	var table = getOffers(klinisch);
+	
+	document.getElementById("botResult").innerHTML = text +"\n"+ table;
+}
+
+function getOffers(klinisch)
+{
+	var tableText = "<table><tr><th>Name</th><th>Kurzbeschreibung</th><th>Kontaktform</th><th>Bezahlung</th><th>Link</th></tr>"
+	
+	for(angebot of angebote)
+	{
+		if((klinisch && (angebot.hilfsangeboteKlinisch)==1)
+			|| (!klinisch && (angebot.hilfsangeboteKlinisch)==0))
+			tableText = tableText + "<tr>"
+					+"<td>"+angebot.name+"</td>"
+					+"<td>"+angebot.description+"</td>"
+					+"<td>"+angebot.contact+"</td>"
+					+"<td>"+angebot.cost+"</td>"
+					+"<td><a class=\"linkgruen\" href=\""+angebot.link+"\">zum Angebot</a></td>"
+				+"</tr>";			
 	}
 	
-	document.getElementById("botResult").innerHTML = text;
+	tableText = tableText+"</table>";
+	
+	return tableText;
 }
+
+
 
 function initQuestionnaire(){
 	

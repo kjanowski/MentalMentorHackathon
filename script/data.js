f075506aba67632097fb894297e5cbb0c9ba5b5c
Kathrin Janowski <science@kathrinjanowski.com>
2021-03-21 10:10:54 +0100

    Grundgerüst

---
+++ script/data.js
@@ -0,0 +1,20 @@
+var angebote = [
+	{	
+		name: "Angebot 1",
+		description: "dies ist das erste Angebot",
+		type: "online",
+		cost: "kostenlos"
+	},
+	{	
+		name: "Angebot 2",
+		description: "dies ist das zweite Angebot",
+		type: "telefonisch",
+		cost: "kostenlos"
+	},
+	{	
+		name: "Angebot 3",
+		description: "dies ist das zweite Angebot",
+		type: "Präsenz",
+		cost: "Krankenkasse"
+	}
+];

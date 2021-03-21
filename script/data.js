c14b5ae6534f112ddb08f92025dc7c2d7889315a
AleBu1 <alexbusch95@gmx.de>
2021-03-21 11:35:19 +0100

    Update data.js

--- script/data.js
+++ script/data.js
@@ -1,9 +1,23 @@
 var angebote = [
 	{	
-		name: "Angebot 1",
-		description: "dies ist das erste Angebot",
-		type: "online",
-		cost: "kostenlos"
+	name:	"Patient:innenservice (116117) und Ärzt:innen-Suche der Kassenärztlichen Bundesvereinigung (KBV)",
+	description:	"In einer Psychotherapie kannst du mit einer speziell dafür ausgebildeten Person (psychologische oder ärztliche Psychotherapeut:innen) über deine Probleme oder Sorgen sprechen. Diese Behandlung kann dir helfen, wenn du merkst, dass du deine täglichen Aufgaben z. B. im Haushalt oder bei der Arbeit nicht mehr schaffst. Wenn du überlegst, eine Psychotherapie zu machen, kannst du über die Website der KBV (Kassenärztliche Bundesvereinigung) herausfinden, wo es Psychotherapeut:innen in deiner Nähe gibt. Über die Termin-Servicestellen kannst du telefonisch unter der Nummer 116117 einen Termin für eine psychotherapeutische Sprechstunde vereinbaren.",
+	link:	"https://www.kbv.de/html/arztsuche.php",
+	cost:	2,
+	location:	3,
+	hilfsangeboteKlinisch:	1,
+	hilfsangeboteSubklinisch:	0,
+	information:	1,
+	praevention:	0,
+	psychoedukation:	0,
+	einzelangebot:	0,
+	website_PDF:	1,
+	app:	0,
+	hotline:	1,
+	vermittlung:	1,
+	beratung:	0,
+	austausch:	0,
+	contact:	"offline",
 	},
 	{	
 		name: "Angebot 2",

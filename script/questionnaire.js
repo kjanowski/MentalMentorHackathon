3311f5de42d547705b28613cb9d0924040d0acd6
Kathrin Janowski <science@kathrinjanowski.com>
2021-03-21 14:28:59 +0100

    Ausschluss von Kategorien

--- script/questionnaire.js
+++ script/questionnaire.js
@@ -5,12 +5,17 @@ gatheredInfo.questions['gad_01'] = 0;
 gatheredInfo.questions['gad_02'] = 0;
 gatheredInfo.questions['phq_01'] = 0;
 gatheredInfo.questions['phq_02'] = 0;
-gatheredInfo.exclude = [];
 
-questionIDs = ['gad_01', 'gad_02', 'phq_01', 'phq_02']
+gatheredInfo.exclude = [];
+gatheredInfo.exclude['online'] = false;
+gatheredInfo.exclude['hotline'] = false;
+
+
+//Schwellwerte für Score
 gadCutoff = 3;
 phqCutoff = 3;
 
+//Fragen-Zähler
 var counter=1;
 
 function updateQuestion(questionID, score)
@@ -18,38 +23,122 @@ function updateQuestion(questionID, score)
 	gatheredInfo.questions[questionID] = score;
 }
 
+function updateExclusion(){
+	gatheredInfo.exclude['online'] = document.getElementById("exclude_online").checked;
+	gatheredInfo.exclude['hotline'] = document.getElementById("exclude_hotline").checked;
+}
+
 function showResult()
 {
-	document.getElementById("frage_"+counter).style.display="none";
+	moveToNext(); //letzte Frage ausblenden und Zähler hinter das Ende setzen 
 
 	var gadScore = gatheredInfo.questions['gad_01']+gatheredInfo.questions['gad_02'];
 	var phqScore = gatheredInfo.questions['phq_01']+gatheredInfo.questions['phq_02'];
 	var klinisch = (gadScore>=gadCutoff) || (phqScore>=phqCutoff);
+	var subklinisch = ((gadScore>0)&&(gadScore<gadCutoff)) || ((phqScore>0)&&(phqScore<phqCutoff));
 	
-	var text = "GAD score: "+gadScore+", PHQ score: "+phqScore+" -> klinisch: "+klinisch;
 	
-	var table = getOffers(klinisch);
+	var text = "Hier sind die Angebote, die ich für dich gefunden habe.";
+	//"GAD score: "+gadScore+", PHQ score: "+phqScore+" -> klinisch: "+klinisch;
+	
+	var table = getOffers(klinisch, subklinisch);
 	
 	document.getElementById("botResult").innerHTML = text +"\n"+ table;
 }
 
-function getOffers(klinisch)
+function isIncluded(angebot){
+	if(gatheredInfo.exclude['online'])
+	{
+		if((angebot.art_website_pdf == 1) || (angebot.contact == "online"))
+			return false;
+	}
+	
+	if(gatheredInfo.exclude['hotline'])
+	{
+		if(angebot.art_hotline == 1)
+			return false;
+	}
+	
+	return true;
+}
+
+
+function getOffers(klinisch, subklinisch)
 {
-	var tableText = "<table><tr><th>Name</th><th>Kurzbeschreibung</th><th>Kontaktform</th><th>Bezahlung</th><th>Link</th></tr>"
+	//Tabellen-Header
+	var tableText = "<table><tr><th>Name</th><th>Kurzbeschreibung</th><th>Art des Angebots</th><th>Bezahlung</th><th>Link</th></tr>"
 	
 	for(angebot of angebote)
 	{
-		if((klinisch && (angebot.hilfsangeboteKlinisch)==1)
-			|| (!klinisch && (angebot.hilfsangeboteKlinisch)==0))
-			tableText = tableText + "<tr>"
-					+"<td>"+angebot.name+"</td>"
-					+"<td>"+angebot.description+"</td>"
-					+"<td>"+angebot.contact+"</td>"
-					+"<td>"+angebot.cost+"</td>"
-					+"<td><a class=\"linkgruen\" href=\""+angebot.link+"\">zum Angebot</a></td>"
-				+"</tr>";			
+		//zuerst auf Ausschluss von Kategorien prüfen
+		if(isIncluded(angebot))
+		{
+			//prüfen, ob das Angebot zum Score passt
+			var matchKlinisch = klinisch && (angebot.hilfsangeboteKlinisch==1);
+			var matchSubklinisch = subklinisch && (angebot.hilfsangeboteSubklinisch==1);
+			var matchNichtklinisch = !(klinisch || subklinisch) && ((angebot.hilfsangeboteKlinisch==0)||(angebot.hilfsangeboteSubklinisch==1));
+				
+			if(matchKlinisch || matchSubklinisch || matchNichtklinisch)
+			{
+				
+				//Angebots-Art(en) ausgeben
+				var angebotsArt = "";
+				
+				if(angebot.art_einzelangebot == 1)
+					angebotsArt += "Einzelangebot";
+				
+				if(angebot.art_website_pdf == 1)
+				{
+					if(angebotsArt.length>0) angebotsArt +=", ";
+					angebotsArt += "Website/PDF";
+				}
+				
+				if(angebot.art_app == 1)
+				{
+					if(angebotsArt.length>0) angebotsArt +=", ";
+					angebotsArt += "App";
+				}
+				
+				if(angebot.art_hotline == 1)
+				{
+					if(angebotsArt.length>0) angebotsArt +=", ";
+					angebotsArt += "Hotline";
+				}
+				
+				if(angebot.art_vermittlung == 1)
+				{
+					if(angebotsArt.length>0) angebotsArt +=", ";
+					angebotsArt += "Vermittlung";
+				}
+
+				if(angebot.art_beratung == 1)
+				{
+					if(angebotsArt.length>0) angebotsArt +=", ";
+					angebotsArt += "Beratung";
+				}
+
+				if(angebot.art_austausch == 1)
+				{
+					if(angebotsArt.length>0) angebotsArt +=", ";
+					angebotsArt += "Austausch";
+				}
+				
+				//Kostenart (1 abziehen, um Index in der Mapping-Liste zu erhalten)
+				var cost = cost_map[angebot.cost-1];
+				
+				//Tabellen-Zeile hinzufügen
+				tableText = tableText + "<tr>"
+						+"<td class=\name\">"+angebot.name+"</td>"
+						+"<td class=\"beschreibung\">"+angebot.description+"</td>"
+						+"<td class=\"art\">"+angebotsArt+"</td>"
+						+"<td class=\"kosten\">"+cost+"</td>"
+						+"<td class=\"link\"><a class=\"linkgruen\" href=\""+angebot.link+"\">zum Angebot</a></td>"
+					+"</tr>";		
+			}
+		}
 	}
 	
+	//Tabelle abschliessen
 	tableText = tableText+"</table>";
 	
 	return tableText;
@@ -58,28 +147,36 @@ function getOffers(klinisch)
 
 
 function initQuestionnaire(){
-	
+	counter=1;
 	document.getElementById("frage_1").style.display="block";
 	document.getElementById("frage_2").style.display="none";
 	document.getElementById("frage_3").style.display="none";
 	document.getElementById("frage_4").style.display="none";
-
+	document.getElementById("frage_5").style.display="none";
 }
 
 function moveToNext()
 {
-	document.getElementById("frage_"+counter).style.display="none";
+	var currBlock = document.getElementById("frage_"+counter);
+	if(currBlock != none)
+		currBlock.style.display="none";
 	
 	counter++;
 
-	document.getElementById("frage_"+counter).style.display="block";	
+	var nextBlock =	document.getElementById("frage_"+counter);
+	if(currBlock != none)
+		currBlock.style.display="block";	
 }
 
 function moveBack()
 {
-	document.getElementById("frage_"+counter).style.display="none";
+	var currBlock = document.getElementById("frage_"+counter);
+	if(currBlock != none)
+		currBlock.style.display="none";
 	
 	counter--;
 
-	document.getElementById("frage_"+counter).style.display="block";	
+	var nextBlock =	document.getElementById("frage_"+counter);
+	if(currBlock != none)
+		currBlock.style.display="block";	
 }

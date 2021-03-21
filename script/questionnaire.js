e45b4d2380ceeed4d7016af5393079b30b8b7590
Kathrin Janowski <science@kathrinjanowski.com>
2021-03-21 12:15:18 +0100

    Fragebogen

--- script/questionnaire.js
+++ script/questionnaire.js
@@ -9,6 +9,7 @@ gatheredInfo.exclude = [];
 
 questionIDs = ['gad_01', 'gad_02', 'phq_01', 'phq_02']
 
+var counter=1;
 
 function updateQuestion(questionID, score)
 {
@@ -17,10 +18,39 @@ function updateQuestion(questionID, score)
 
 function showResult()
 {
+	document.getElementById("frage_"+counter).style.display="none";
+	
 	var text = "score:";
 	for(id of questionIDs){
 		text = text+"\n"+id+": "+gatheredInfo.questions[id];
 	}
 	
 	document.getElementById("botResult").innerHTML = text;
+}
+
+function initQuestionnaire(){
+	
+	document.getElementById("frage_1").style.display="block";
+	document.getElementById("frage_2").style.display="none";
+	document.getElementById("frage_3").style.display="none";
+	document.getElementById("frage_4").style.display="none";
+
+}
+
+function moveToNext()
+{
+	document.getElementById("frage_"+counter).style.display="none";
+	
+	counter++;
+
+	document.getElementById("frage_"+counter).style.display="block";	
+}
+
+function moveBack()
+{
+	document.getElementById("frage_"+counter).style.display="none";
+	
+	counter--;
+
+	document.getElementById("frage_"+counter).style.display="block";	
 }

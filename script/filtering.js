96049128e0ab6ca58b2e3375de46208d03ff2bb9
Kathrin Janowski <science@kathrinjanowski.com>
2021-05-09 11:26:32 +0200

    script

--- script/filtering.js
+++ script/filtering.js
@@ -1,13 +1,13 @@
-filters = [];
-filters[0]= { name:"Telefonisches Angebot", active:false };
-filters[1]= { name:"Psychotherapievermittlung", active:false };
+filters_type = [];
+filters_type[0]= { displayName:"Telefonisches Angebot", internalName:"art_hotline", active:false };
+filters_type[1]= { displayName:"Psychotherapievermittlung", internalName:"art_vermittlung", active:false };
 
 
 function isFiltered(){
 	var i=0;
-	while(i<filters.length)
+	while(i<filters_type.length)
 	{
-		if(filters[i].active)
+		if(filters_type[i].active)
 			return true;
 	}
 	return false;
@@ -18,5 +18,18 @@ function isIncludedInFilter(angebot){
 	if(!isFiltered)
 		return true;	//wenn kein Filter aktiv ist, ist Angebot automatisch verfügbar
 	
-	
+	var i=0;
+	var included = false;
+	while((i<filters_type.length) || (!included))
+	{
+		var internalName = filters_type[i].internalName; //Anzeigename auf Feld in der Datenstruktur abbilden
+		
+		//sobald ein aktiver Filter mit dem Angebot übereinstimmt, ist es eingeschlossen
+		included = included || (filters_type[i].active && (angebot[internalName] == 1))
+		
+		i++;
+	}
+	//included ist true, falls eines der "Art des Angebots"-Tags zutrifft
+		
+	return included;
 }
